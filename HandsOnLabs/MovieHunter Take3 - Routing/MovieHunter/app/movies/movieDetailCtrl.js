7e8e2901690ff0ed5ce6baf3f060585008deec32
DeborahK <deborahk@insteptech.com>
2015-06-15 14:58:05 -0700

    Checked in rough draft of remaining hands on labs

---
+++ HandsOnLabs/MovieHunter Take3 - Routing/MovieHunter/app/movies/movieDetailCtrl.js
@@ -0,0 +1,25 @@
+﻿(function () {
+    "use strict";
+
+    angular
+        .module("movieHunter")
+        .controller("MovieDetailCtrl",
+                    ["movieResource",
+                        "$routeParams",
+                     MovieDetailCtrl]);
+
+    function MovieDetailCtrl(movieResource, $routeParams) {
+        var vm = this;
+
+        vm.movieId = $routeParams.movieId;
+        vm.movie = {
+            movieId: 1,
+            title: 'The Fellowship of the Ring',
+            director: 'Jackson',
+            releaseDate: '2001-12-19',
+            imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Fellowship-Of-The-Ring_small.jpg',
+            mpaa: 'pg-13'
+        };
+
+    }
+}());
---
+++ HandsOnLabs/MovieHunter Take3 - Routing/MovieHunter/app/movies/movieListCtrl.js
@@ -0,0 +1,48 @@
+﻿(function () {
+    "use strict";
+
+    angular
+    .module("movieHunter")
+    .controller("MovieListCtrl",
+                ["movieResource",
+                    MovieListCtrl]);
+
+    function MovieListCtrl(movieResource) {
+        var vm = this;
+
+        vm.movies = [
+            {
+                movieId: 1,
+                title: 'The Fellowship of the Ring',
+                director: 'Jackson',
+                releaseDate: '2001-12-19',
+                imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Fellowship-Of-The-Ring_small.jpg',
+                mpaa: 'pg-13'
+            },
+            {
+                movieId: 2,
+                title: 'The Two Towers',
+                director: 'Jackson',
+                releaseDate: '2002-12-18',
+                imageurl: 'http://www.coverwhiz.com/content/The-Lord-Of-The-Rings-The-Two-Towers_small.jpg',
+                mpaa: 'pg-13'
+            },
+            {
+                movieId: 3,
+                title: 'The Point',
+                director: 'Wolf',
+                releaseDate: '1971-02-02',
+                imageurl: '',
+                mpaa: 'nr'
+            }
+        ];
+        vm.title = "Search by Movie Title";
+        vm.showImage = false;
+
+        vm.toggleImage = function () {
+            vm.showImage = !vm.showImage;
+        };
+    }
+
+}());
+
